40667b7af6981e3816cf8c64ee18f8bef3a66073
jlntriw <juliantowibowo3@gmail.com>
2022-10-21 21:48:27 +0700

    Update cuaca

--- Main.py
+++ Main.py
@@ -14,3 +14,4 @@ if __name__ == '__main__' :
     print('Aplikasi Cuaca di Temanggung')
     result = Perkiraan_Cuaca.ekstraksi_data()
     Perkiraan_Cuaca.tampilkan_data(result)
+    print('thanks')

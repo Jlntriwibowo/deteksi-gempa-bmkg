a763b4806563b008efc1bdbb84fe8519c3dd771f
jlntriw <juliantowibowo3@gmail.com>
2022-10-21 21:39:14 +0700

    Perkiraan Cuaca temanggung

--- Gempa_terkini/__init__.py
+++ Gempa_terkini/__init__.py
@@ -1,7 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
 def ekstraksi_data():
     """
 tanggal     : 16 Oktober 2022
-waktu       : 08:13:24 WIB
+waktu       : 08:13pip:24 WIB
 magnitudo   :4.5
 kedalaman   : 10 km
 Koordinat   : 8.73 LS - 116.62 BT
@@ -9,19 +12,60 @@ Pusat Gempa : Pusat gempa berada di laut 13 km Tenggara Lombok Timur
 Dirasakan (Skala MMI): III-IV Lombok Timur, III Lombok Barat, III Mataram, III Lombok Tengah, III Sumbawa Barat
  :return:
     """
-    hasil = dict()
-    hasil['tanggal'] = '16 Oktober 2022'
-    hasil['waktu'] = '08:13:24 WIB'
-    hasil['magnitudo'] = 4.5
-    hasil['kedalaman'] = '10 km'
-    hasil['koordinat'] = '8.73 LS - 116.62 BT'
-    hasil['pusat gempa'] = 'Pusat gempa berada di laut 13 km Tenggara Lombok Timur'
-    hasil['dirasakan'] = \
-        '(Skala MMI): III-IV Lombok Timur, III Lombok Barat, III Mataram, III Lombok Tengah, III Sumbawa Barat'
-    return hasil
+#menghindari error menjadi None
 
+    try:
+        content = requests.get('https://www.bmkg.go.id')
+    except Exception:
+        return None
 
+# Mencari tanggal dan waktu class sudah jelas
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+        resulttime = soup.find('span',{'class' : 'waktu'})
+        resulttime = resulttime.text.split(', ')
+        tanggal = resulttime[0]
+        waktu = resulttime[1]
+# Mencari Magnitudo dll karena unordered list
+        resultm = soup.find('div', {'class' : "col-md-6 col-xs-6 gempabumi-detail no-padding"})
+        resultm = resultm.findChildren ('li')
+        i=0
+        magnitudo = None
+        kedalaman = None
+        Koordinat = None
+        Pusat_Gempa = None
+        Dirasakan = None
+        for res in resultm:
+            print(i, res)
+            if i == 1:
+                magnitudo = res.text
+            if i == 2 :
+                kedalaman = res.text
+            if i == 3 :
+                koordinat = res.text
+            if i == 4 :
+                Pusat_Gempa = res.text
+            if i == 5 :
+                Dirasakan = res.text
+            i=i+1
+#mencari hasil
+        hasil = dict()
+        hasil['tanggal'] = tanggal #'16 Oktober 2022'
+        hasil['waktu'] = waktu #'08:13:24 WIB'
+        hasil['magnitudo'] = magnitudo #4.5
+        hasil['kedalaman'] = kedalaman #'10 km'
+        hasil['koordinat'] = koordinat #'8.73 LS - 116.62 BT'
+        hasil['pusat gempa'] = Pusat_Gempa #'Pusat gempa berada di laut 13 km Tenggara Lombok Timur'
+        hasil['dirasakan'] = Dirasakan
+            #'(Skala MMI): III-IV Lombok Timur, III Lombok Barat, III Mataram, III Lombok Tengah, III Sumbawa Barat'
+        return hasil
+    else :
+        return None
+#mencetak hasil
 def tampilkan_data(result):
+    if result is None:
+        print('data tidak di temukan')
+        return
     print('Gempa terakhir berdasarkan BMKG')
     print(f"1.tanggal : \n           {result['tanggal']}")
     print(f"2. waktu : \n           {result['waktu']}")
--- Main.py
+++ Main.py
@@ -1,11 +1,16 @@
 """
 Program Deteksi Gempa
 """
-from Gempa_terkini import ekstraksi_data, tampilkan_data
+import Gempa_terkini
 
 if __name__ == '__main__' :
     print('aplikasi utama')
-    result = ekstraksi_data()
-    tampilkan_data(result)
+    result = Gempa_terkini.ekstraksi_data()
+    Gempa_terkini.tampilkan_data(result)
 
-    
+import Perkiraan_Cuaca
+
+if __name__ == '__main__' :
+    print('Aplikasi Cuaca di Temanggung')
+    result = Perkiraan_Cuaca.ekstraksi_data()
+    Perkiraan_Cuaca.tampilkan_data(result)
